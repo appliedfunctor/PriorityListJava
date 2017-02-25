114906195d5d3109bf0e6aeb13e7e07c75b75fe6
Alexander Worton <kesomir@btinternet.com>
2017-02-25 15:39:41 +0000

    Added show stage instruction

--- src/main/java/HelloFx.java
+++ src/main/java/HelloFx.java
@@ -11,9 +11,10 @@ public class HelloFx extends Application{
 
     @Override
     public void start(Stage stage){
-        stage.setTitle("Hello ScalaFX");
+        stage.setTitle("Hello JavaFX");
         Group group = new Group(new Label("Hello Label"));
         Scene scene = new Scene(group, 800, 600);
         stage.setScene(scene);
+        stage.show();
     }
 }
